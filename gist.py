5b484eb9b437e15279cad986d790559fb38f77d5
Spencer Russell <spencer.f.russell@gmail.com>
2013-07-08 21:44:56 -0400

    adds basic gist.py skeleton and requirements.txt

---
+++ gist.py
@@ -0,0 +1,19 @@
+'''gist.py
+
+A command-line gist poster in Python.
+
+This project is mainly a demo of docopt and cmd, two great python libraries for
+making beautiful and functional command-line programs.
+
+Usage: gist.py'''
+
+import docopt
+
+
+def main(args):
+    pass
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    main(args)

09a0a28419c935abc8c2d44835ba6bbcbd4ffe02
Spencer Russell <spencer.f.russell@gmail.com>
2013-07-08 22:54:35 -0400

    refactors gist posting into funtion and adds empty cmd prompt

--- gist.py
+++ gist.py
@@ -5,17 +5,21 @@
 A command-line gist poster in Python.
 
 This project is mainly a demo of docopt and cmd, two great python libraries for
-making beautiful and functional command-line programs.
+making beautiful and functional command-line programs. This utility can be used
+as a quick way to post gists from the command line, or if the user does not
+provide a filename then a command prompt is opened.
 
-Usage: gist.py [-d <desc>] <filename>
+Usage: gist.py [-d <desc>] [<filename>]
 
 Options:
-    -d <desc>   A short description of this gist [default: ]'''
+    -d <desc>   A short description of this gist [default: ]
+    <filename>  A file to be posted as a gist.'''
 
 from __future__ import print_function
 from docopt import docopt
 import requests
 import json
+from cmd import Cmd
 
 GIST_API_URL = 'https://api.github.com/gists'
 
@@ -23,19 +27,32 @@ GIST_API_URL = 'https://api.github.com/gists'
 def main(args):
     gist_filename = args['<filename>']
     gist_desc = args['-d']
-    gist_file = open(gist_filename)
+    if gist_filename is not None:
+        gist_url = post_gist(gist_filename, gist_desc)
+        print(gist_url)
+    else:
+        app = GistCmd()
+        app.cmdloop()
+
+
+def post_gist(filename, description):
+    gist_file = open(filename)
 
     new_gist = {
-        'description': gist_desc,
+        'description': description,
         'public': True,
         'files': {
-            gist_filename: {
+            filename: {
                 'content': gist_file.read()
             }
         }
     }
     response = requests.post(GIST_API_URL, data=json.dumps(new_gist))
-    print(response.json()['html_url'])
+    return response.json()['html_url']
+
+
+class GistCmd(Cmd):
+    pass
 
 
 if __name__ == '__main__':
